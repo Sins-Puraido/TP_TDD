1bf54bb09220d2e97595a97c30b148ee39c3398a
Sins-Puraido <guillaume.dudouet0@gmail.com>
2025-02-26 16:11:00 +0100

    Initial commit

---
+++ src/main/java/tp/dudouetg/tp/TpTddApplication.java
@@ -0,0 +1,13 @@
+package tp.dudouetg.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tp/dudouetg/tp/TpTddApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.dudouetg.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
